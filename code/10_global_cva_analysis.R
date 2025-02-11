a840258ce422122247ab32e67106f047f213ff78
akmiller01 <alex.k.miller@gmail.com>
2025-02-10 21:10:15 -0500

    Deduplication by matching subgrants

--- code/10_global_cva_analysis.R
+++ code/10_global_cva_analysis.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("data.table","tidyverse", "xlsx", "stringr")
+list.of.packages <- c("data.table","tidyverse", "xlsx", "stringr", "stringdist")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 suppressPackageStartupMessages(lapply(list.of.packages, require, character.only=T))
@@ -33,7 +33,7 @@ survey_data$Vouchers.USD = as.numeric(survey_data$Vouchers.USD)
 survey_data$CVAamount =
   rowSums(survey_data[,c("Cash.USD", "Vouchers.USD")], na.rm=T)
 sub_grants = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheetIndex=2)
-sub_grants = subset(sub_grants, tolower(Take.out)=="n")
+sub_grants = subset(sub_grants, tolower(Take.out)=="y")
 pc_tv_estimate = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheetIndex=3)
 
 # Reads in the fts_survey_overlap to use as mapping, overlap calculated by survey data automatically
@@ -81,3 +81,230 @@ cva_agg = merge(
   by="destinationObjects_Organization.organizationSubTypes",
   all.x=T
 )
+
+# Match subgrant names
+quotemeta <- function(string) {
+  str_replace_all(string, "(\\W)", "\\\\\\1")
+}
+
+remove_punct = function(string){
+  str_replace_all(string, "[[:punct:]]", " ")
+}
+
+collapse_whitespace = function(string){
+  str_replace_all(string, "\\s+", " ")
+}
+sub_grants$clean_name = str_trim(collapse_whitespace(remove_punct(tolower(sub_grants$Recipient.org))))
+sub_grants$clean_name[
+  which(sub_grants$clean_name %in% c("unknown", "not provided potentially sensitive"))
+] = NA
+unique_subgrant_recipients = unique(sub_grants$clean_name)
+unique_subgrant_recipients = unique_subgrant_recipients[which(!unique_subgrant_recipients %in% c(NA, ""))]
+cva_agg$clean_org = str_trim(collapse_whitespace(remove_punct(tolower(cva_agg$Organisation))))
+unique_org_names = unique(cva_agg$clean_org)
+unique_org_names = unique_org_names[which(!unique_org_names %in% c(NA, ""))]
+
+match_df = data.frame(subgrant_recipient_org=unique_subgrant_recipients)
+match_df$perfect_match_name = NA
+match_df$fuzzy_match_name = NA
+match_df$fuzzy_match_distance = NA
+match_df$substring_a_match_name = NA
+match_df$substring_b_match_name = NA
+
+# Try match on exact name
+for(i in 1:nrow(match_df)){
+  org_name = match_df[i,"subgrant_recipient_org"]
+  if(org_name %in% unique_org_names){
+    match_index = which(unique_org_names==org_name)
+    match_name = unique_org_names[match_index]
+    match_df$perfect_match_name[i] = match_name
+  }
+}
+
+# Percentage matched by exact name
+sum(!is.na(match_df$perfect_match_name)) / nrow(match_df)
+
+# Match by string distance
+allowable_percentage_difference = 0.20 # 20% of the string
+for(i in 1:nrow(match_df)){
+  org_name = match_df[i,"subgrant_recipient_org"]
+  allowable_difference = ceiling(allowable_percentage_difference * nchar(org_name))
+  allowable_difference = max(allowable_difference, 1) # minimum 1 character
+  distance_vector = stringdist(org_name, unique_org_names)
+  matches_by_distance = which(distance_vector <= allowable_difference)
+  if(length(matches_by_distance) >= 1){
+    distances = distance_vector[matches_by_distance]
+    match_index = matches_by_distance[which.min(distances)] # Shortest distance
+    match_name = unique_org_names[match_index]
+    match_df$fuzzy_match_name[i] = match_name
+    match_df$fuzzy_match_distance[i] = distance_vector[match_index]
+  }
+}
+
+# Percentage matched by distance
+sum(!is.na(match_df$fuzzy_match_name)) / nrow(match_df)
+
+# Joint percentage matched so far
+(
+  sum(!is.na(match_df$perfect_match_name)| !is.na(match_df$fuzzy_match_name))
+) / nrow(match_df)
+
+fuzz = subset(match_df, !is.na(fuzzy_match_name))
+fuzz = fuzz[order(-fuzz$fuzzy_match_distance),]
+View(fuzz[,c("subgrant_recipient_org", "fuzzy_match_name", "fuzzy_match_distance")])
+# Manual fix incorrect fuzzy match
+match_df$fuzzy_match_name[which(match_df$subgrant_recipient_org=="drc")] = NA
+
+# Match on subgrant org being a substring of org
+for(i in 1:nrow(match_df)){
+  org_name = match_df[i,"subgrant_recipient_org"]
+  org_regex = paste0(
+    "\\b",
+    quotemeta(org_name),
+    "\\b"
+  )
+  substring_matches = which(grepl(org_regex, unique_org_names, perl=T))
+  if(length(substring_matches) >= 1){
+    all_matching_names = unique_org_names[substring_matches]
+    name_lengths = nchar(all_matching_names)
+    match_index = substring_matches[which.min(name_lengths)]
+    match_name = unique_org_names[match_index]
+    match_df$substring_a_match_name[i] = match_name
+  }
+}
+
+
+# Percentage matched by substring a
+sum(!is.na(match_df$substring_a_match_name)) / nrow(match_df)
+
+# Joint percentage matched so far
+(
+  sum(!is.na(match_df$perfect_match_name) |
+        !is.na(match_df$fuzzy_match_name) |
+        !is.na(match_df$substring_a_match_name)
+  )
+) / nrow(match_df)
+
+# Match on org being a substring of subgrant org for remaining unmatched
+match_df$unmatched = is.na(match_df$perfect_match_name) &
+  is.na(match_df$fuzzy_match_name) &
+  is.na(match_df$substring_a_match_name)
+
+for(match_index in 1:length(unique_org_names)){
+  match_name = unique_org_names[match_index]
+  match_regex = paste0(
+    "\\b",
+    quotemeta(match_name),
+    "\\b"
+  )
+  substring_matches = which(grepl(match_regex, match_df$subgrant_recipient_org, perl=T))
+  if(length(substring_matches) >= 1){
+    for(i in substring_matches){
+      if(match_df$unmatched[i]){
+        match_df$substring_b_match_name[i] = match_name
+      }
+    }
+  }
+}
+
+
+# Percentage matched by substring b
+sum(!is.na(match_df$substring_b_match_name)) / nrow(match_df)
+
+# Joint percentage matched so far
+(
+  sum(!is.na(match_df$perfect_match_name) |
+        !is.na(match_df$fuzzy_match_name) |
+        !is.na(match_df$substring_a_match_name) |
+        !is.na(match_df$substring_b_match_name)
+  )
+) / nrow(match_df)
+
+# Manual matches
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="care bangladesh")
+] = "care international"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="wfp")
+] = "world food programme"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="save the childrensave the children")
+] = "save the children"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="wvi")
+] = "world vision international"
+match_df$perfect_match_name[
+  which(grepl("world vision|vision mund", match_df$subgrant_recipient_org))
+] = "world vision international"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="acf")
+] = "action against hunger"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="action contre la faim espagne")
+] = "action against hunger"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="cww")
+] = "concern worldwide"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="dan church aid")
+] = "dca"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="drc")
+] = "danish refugee council"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="norwegian refugee council")
+] = "nrc"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="pin")
+] = "people in need"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="unrwa")
+] = "united nations relief and works agency for palestine refugees in the near east"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="unrwa united nations relief and wor")
+] = "united nations relief and works agency for palestine refugees in the near east"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="the united nations relief and works")
+] = "united nations relief and works agency for palestine refugees in the near east"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="united nations children s fund")
+] = "unicef"
+match_df$perfect_match_name[
+  which(grepl("red (cross|crescent)", match_df$subgrant_recipient_org))
+] = "red cross and red crescent movement"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="plan malawi")
+] = "plan international"
+match_df$perfect_match_name[
+  which(match_df$subgrant_recipient_org=="adra romania")
+] = "adventist development and relief agency"
+match_df$unmatched = is.na(match_df$perfect_match_name) &
+  is.na(match_df$fuzzy_match_name) &
+  is.na(match_df$substring_a_match_name) &
+  is.na(match_df$substring_b_match_name)
+View(subset(match_df, unmatched, select="subgrant_recipient_org"))
+
+subgrant_org_mapping = match_df$perfect_match_name
+subgrant_org_mapping[which(is.na(subgrant_org_mapping))] = 
+  match_df$fuzzy_match_name[which(is.na(subgrant_org_mapping))]
+subgrant_org_mapping[which(is.na(subgrant_org_mapping))] = 
+  match_df$substring_a_match_name[which(is.na(subgrant_org_mapping))]
+subgrant_org_mapping[which(is.na(subgrant_org_mapping))] = 
+  match_df$substring_b_match_name[which(is.na(subgrant_org_mapping))]
+names(subgrant_org_mapping) = match_df$subgrant_recipient_org
+sub_grants$clean_org = subgrant_org_mapping[sub_grants$clean_name]
+
+# Aggregate
+sub_grants_agg = data.table(sub_grants)[
+  ,.(CVAamount_subgrant=sum(Amount.USD, na.rm=T)),
+  by=.(clean_org, Year)
+]
+sub_grants_agg = subset(sub_grants_agg, !is.na(clean_org))
+setnames(sub_grants_agg, "Year", "year")
+
+# Merge and subtract
+cva_agg = merge(cva_agg, sub_grants_agg, by=c("clean_org", "year"), all.x=T)
+cva_agg$CVAamount_subgrant[which(is.na(cva_agg$CVAamount_subgrant))] = 0
+cva_agg$CVAamount_undoubled = cva_agg$CVAamount - cva_agg$CVAamount_subgrant
+cva_agg$CVAamount_undoubled = pmax(cva_agg$CVAamount_undoubled, 0)
+cva_agg_org_type = cva_agg[,.(CVAamount=sum(CVAamount_undoubled)), by=.(year, cva_org_type)]
