2c6eef3b84df0b348d49f4706654c2ac80e72cbb
akmiller01 <alex.k.miller@gmail.com>
2025-02-11 11:37:57 -0500

    Global CVA

--- code/10_global_cva_analysis.R
+++ code/10_global_cva_analysis.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("data.table","tidyverse", "xlsx", "stringr", "stringdist")
+list.of.packages <- c("data.table","tidyverse", "openxlsx", "stringr", "stringdist")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 suppressPackageStartupMessages(lapply(list.of.packages, require, character.only=T))
@@ -23,18 +23,18 @@ setwd("..")
 # Read previous output
 fts_cva = fread("output/fts_cva.csv")
 
+# Exclude large confidental flows
+fts_cva = subset(fts_cva, destinationObjects_Organization.name!="International NGOs (Confidential)")
+
 # Reads in the CVA survey data
-survey_data = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheetIndex=1)
+survey_data = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheet=1)
 survey_data$Organisation = str_trim(survey_data$Organisation)
 survey_data$PC.USD.m = as.numeric(survey_data$PC.USD.m)
 survey_data$TV.USD.m = as.numeric(survey_data$TV.USD.m)
-survey_data$Cash.USD = as.numeric(survey_data$Cash.USD)
-survey_data$Vouchers.USD = as.numeric(survey_data$Vouchers.USD)
-survey_data$CVAamount =
-  rowSums(survey_data[,c("Cash.USD", "Vouchers.USD")], na.rm=T)
-sub_grants = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheetIndex=2)
+sub_grants = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheet=2)
 sub_grants = subset(sub_grants, tolower(Take.out)=="y")
-pc_tv_estimate = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheetIndex=3)
+pc_tv_estimate = read.xlsx("reference_datasets/cva_survey_data.xlsx", sheet=3)
+setnames(pc_tv_estimate, "CVA.data.year", "year")
 
 # Reads in the fts_survey_overlap to use as mapping, overlap calculated by survey data automatically
 fts_survey_overlap = fread("reference_datasets/fts_survey_overlap.csv", header=T)
@@ -49,38 +49,38 @@ survey_overlap_combinations = paste(fts_survey_overlap_long$destinationObjects_O
 
 # Reads in the attached cva_org_type file so that it can be joined with fts_cva by “destinationObjects_Organization.organizationSubTypes”
 cva_org_type = fread("reference_datasets/cva_org_type.csv")
+setnames(cva_org_type, "cva_org_type", "Org_type")
 
 # Aggregate FTS for joining
-sub_type_mapping = unique(fts_cva[,c("destinationObjects_Organization.name", "destinationObjects_Organization.organizationSubTypes")])
-fts_cva_agg = fts_cva[,.(CVAamount=sum(CVAamount)), by=.(
+fts_cva_agg = fts_cva[,.(PC.USD.m=sum(CVAamount) / 1e6), by=.(
   year,
-  destinationObjects_Organization.name
+  destinationObjects_Organization.name,
+  destinationObjects_Organization.organizationSubTypes
 )]
 fts_cva_agg$source = "FTS"
 fts_cva_agg = merge(fts_cva_agg, name_mapping, by="destinationObjects_Organization.name", all.x=T)
+fts_cva_agg = merge(fts_cva_agg, cva_org_type, by="destinationObjects_Organization.organizationSubTypes", all.x=T)
 
 # Remove surveyed years
 fts_cva_agg$org_year = paste(fts_cva_agg$destinationObjects_Organization.name, fts_cva_agg$year)
 fts_cva_agg = subset(fts_cva_agg, !org_year %in% survey_overlap_combinations)
 fts_cva_agg$org_year = NULL
 
-# Add survey CVA
-survey_data = merge(survey_data, name_mapping, by="Organisation", all.x=T)
-survey_data_harmonized = survey_data[,c("Year", "destinationObjects_Organization.name", "Organisation", "CVAamount")]
-setnames(survey_data_harmonized, "Year", "year")
-survey_data_harmonized$source = "Survey"
-cva_agg = rbind(fts_cva_agg, survey_data_harmonized)
+# Impute TV
+fts_cva_agg = merge(fts_cva_agg, pc_tv_estimate, by="year", all.x=T)
+fts_cva_agg$TV.USD.m = fts_cva_agg$PC.USD.m * fts_cva_agg$PC.average.used
+fts_cva_agg$PC.average.used = NULL
+
+# Add FTS to survey CVA
+setnames(
+  fts_cva_agg,
+  c("year"),
+  c("Year")
+)
+survey_data$source = "Survey"
+cva_agg = rbindlist(list(survey_data, fts_cva_agg), fill=T)
 cva_agg$Organisation[which(is.na(cva_agg$Organisation))] = 
   cva_agg$destinationObjects_Organization.name[which(is.na(cva_agg$Organisation))]
-
-# Merge org types
-cva_agg = merge(cva_agg, sub_type_mapping, by="destinationObjects_Organization.name", all.x=T)
-cva_agg = merge(
-  cva_agg, 
-  cva_org_type, 
-  by="destinationObjects_Organization.organizationSubTypes",
-  all.x=T
-)
 
 # Match subgrant names
 quotemeta <- function(string) {
@@ -296,15 +296,22 @@ sub_grants$clean_org = subgrant_org_mapping[sub_grants$clean_name]
 
 # Aggregate
 sub_grants_agg = data.table(sub_grants)[
-  ,.(CVAamount_subgrant=sum(Amount.USD, na.rm=T)),
+  ,.(PC.USD.m_subgrant=sum(Amount.USD, na.rm=T)),
   by=.(clean_org, Year)
 ]
 sub_grants_agg = subset(sub_grants_agg, !is.na(clean_org))
-setnames(sub_grants_agg, "Year", "year")
 
 # Merge and subtract
-cva_agg = merge(cva_agg, sub_grants_agg, by=c("clean_org", "year"), all.x=T)
-cva_agg$CVAamount_subgrant[which(is.na(cva_agg$CVAamount_subgrant))] = 0
-cva_agg$CVAamount_undoubled = cva_agg$CVAamount - cva_agg$CVAamount_subgrant
-cva_agg$CVAamount_undoubled = pmax(cva_agg$CVAamount_undoubled, 0)
-cva_agg_org_type = cva_agg[,.(CVAamount=sum(CVAamount_undoubled)), by=.(year, cva_org_type)]
+cva_agg = merge(cva_agg, sub_grants_agg, by=c("clean_org", "Year"), all.x=T)
+cva_agg$PC.USD.m_subgrant[which(is.na(cva_agg$PC.USD.m_subgrant))] = 0
+cva_agg$PC.USD.m_undoubled = cva_agg$PC.USD.m - cva_agg$PC.USD.m_subgrant
+cva_agg$PC.USD.m_undoubled = pmax(cva_agg$PC.USD.m_undoubled, 0)
+cva_agg_org_type = cva_agg[,.(PC.USD.m=sum(PC.USD.m_undoubled, na.rm=T)), by=.(Year, Org_type)]
+setnames(pc_tv_estimate, "year", "Year")
+cva_agg_org_type = merge(cva_agg_org_type, pc_tv_estimate, by="Year", all.x=T)
+cva_agg_org_type$TV.USD.m = cva_agg_org_type$PC.USD.m * cva_agg_org_type$PC.average.used
+cva_agg_org_type$PC.average.used = NULL
+
+# Write
+fwrite(cva_agg, "output/cva_agg.csv")
+fwrite(cva_agg_org_type, "output/cva_agg_org_type.csv")
